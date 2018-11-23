3476d15438742f20f05711211ce483fff2f4f459
Akansha Vashisth <akanshavashisth01@gmail.com>
2018-11-23 11:20:42 -0800

    Changed file location

--- scripts/read_clean.py
+++ scripts/read_clean.py
@@ -11,19 +11,20 @@ parser = argparse.ArgumentParser()
 
 # adding argument for input file
 parser.add_argument('input_file')
+parser.add_argument('output_file')
 args = parser.parse_args()
 
 def main():
 
     # read the data
     data = pd.read_csv(args.input_file)
-    print(data.head(10))
 
     # Wrangling data
     #Renaming the column MCP.1
     data.rename(columns={'MCP.1':'MCP1'},
                      inplace=True)
 
+    #Checking for all positive values
     assert len([x for x in data.Age if x < 0]) < 1 , "all values should be postive"
     assert len([x for x in data.BMI if x < 0]) < 1 , "all values should be postive"
     assert len([x for x in data.Glucose if x < 0]) < 1 , "all values should be postive"
@@ -34,6 +35,9 @@ def main():
     assert len([x for x in data.Resistin if x < 0]) < 1 , "all values should be postive"
     assert len([x for x in data.MCP1 if x < 0]) < 1 , "all values should be postive"
 
+
+    #Writing new csv  input_file
+    data.to_csv(args.output_file)
 
     # Summary of all the features
     data.describe()
